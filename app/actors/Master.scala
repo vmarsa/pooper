12fb3792ec79bc676b353d28de09e7498748314f
Dmitry Baskakov <dmitry.baskakov.github@gmail.com>
2014-10-13 14:50:47 +0400

    Plus 6 workers

--- app/actors/Master.scala
+++ app/actors/Master.scala
@@ -29,7 +29,14 @@ class Master(slaveFactory: ActorRefFactory => ActorRef,
       "http://pooper.hostingsiteforfree.com/poop.php",
       "http://pooper.bugs3.com/poop.php",
       "http://pooper.site90.net/poop.php",
-      "http://pooper.1eko.com/poop.php")
+      "http://pooper.1eko.com/poop.php",
+      "http://vmarsa.xhc.ru/poop.php",
+      "http://pooper.bb777.ru/poop.php",
+      "http://www.pooper.seo-vip.com/poop.php",
+      "http://stark-gorge-8713.herokuapp.com/poop.php",
+      "http://pooper.cloudcontrolled.com",
+      "http://pooperxaz.appspot.com/",
+      "http://php-pooper2.rhcloud.com/poop.php")
 
   def this() = this(_.actorOf(Props[Slave], "selfie"),
     (f, url) => f.actorOf(Props(new RemoteSlave(url))),
